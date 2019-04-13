85aaf3d203a4de4cd6463dfd969a74762beebe54
Jonas Galvez <jonasgalvez@gmail.com>
2019-04-13 20:52:14 -0300

    ee-first cleanups

--- src/ee-first.js
+++ src/ee-first.js
@@ -6,19 +6,8 @@
 
 // Create the event listener.
 function listener (event, done) {
-  return function onevent (arg1) {
-    var args = new Array(arguments.length)
-    var ee = this
-    var err = event === 'error'
-      ? arg1
-      : null
-
-    // copy args to prevent arguments escaping scope
-    for (var i = 0; i < args.length; i++) {
-      args[i] = arguments[i]
-    }
-
-    done(err, ee, event, args)
+  return function onevent (...args) {
+    done(event === 'error' ? args[0] : null, this, event, args)
   }
 }
 
@@ -27,51 +16,32 @@ module.exports = function first (stuff, done) {
   if (!Array.isArray(stuff)) {
     throw new TypeError('arg must be an array of [ee, events...] arrays')
   }
-
-  var cleanups = []
-
-  for (var i = 0; i < stuff.length; i++) {
-    var arr = stuff[i]
-
+  const cleanups = []
+  for (const arr of stuff) {
     if (!Array.isArray(arr) || arr.length < 2) {
-      throw new TypeError('each array member must be [ee, events...]')
+      throw new TypeError('Array members must be [ee, ...events]')
     }
-
-    var ee = arr[0]
-
-    for (var j = 1; j < arr.length; j++) {
-      var event = arr[j]
-      var fn = listener(event, callback)
-
-      // listen to the event
-      ee.on(event, fn)
-      // push this listener to the list of cleanups
-      cleanups.push({
-        ee: ee,
-        event: event,
-        fn: fn
-      })
+    const ee = arr[0]
+    let j = 1
+    for (; j < arr.length; j++) {
+      let event = arr[j]
+      let fn = listener(event, callback)
+      ee.on(event, fn) // listen to the event
+      cleanups.push({ ee, event, fn }) // push this listener to the list of cleanups
     }
   }
-
   function callback () {
     cleanup()
     done.apply(null, arguments)
   }
-
   function cleanup () {
-    var x
-    for (var i = 0; i < cleanups.length; i++) {
-      x = cleanups[i]
+    for (const x of cleanups) {
       x.ee.removeListener(x.event, x.fn)
     }
   }
-
   function thunk (fn) {
     done = fn
   }
-
   thunk.cancel = cleanup
-
   return thunk
 }
