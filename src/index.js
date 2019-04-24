ab89e2409f840baff64e0e0acf6f654030843f07
Jonas Galvez <jonasgalvez@gmail.com>
2019-04-24 03:35:34 -0300

    refactor router

--- src/index.js
+++ src/index.js
@@ -26,9 +26,11 @@ export default class Metal extends EventEmitter {
   }
   use (route, handle) {
     // default route to '/'
-    if (typeof route !== 'string') {
+    if (typeof route !== 'string' && !(route instanceof RegExp)) {
       handle = route
       route = '/'
+    } else if (!route instanceof RegExp) {
+      route = new RegExp(route, 'i')
     }
     // wrap sub-apps
     if (typeof handle.handle === 'function') {
@@ -40,59 +42,25 @@ export default class Metal extends EventEmitter {
     if (handle instanceof http.Server) {
       handle = handle.listeners('request')[0]
     }
-    // strip trailing slash
-    if (route[route.length - 1] === '/') {
-      route = route.slice(0, -1)
-    }
     this.stack.push({ route, handle })
     return this
   }
   async handle (req, res, out) {
     let index = 0
-    let protohost
-    let removed = ''
-    let slashAdded = false
     let stack = this.stack
     req.originalUrl = req.originalUrl || req.url
     let done = out || handler(req, res, { env, onerror })
     function next (err) {
-      if (slashAdded) {
-        req.url = req.url.substr(1)
-        slashAdded = false
-      }
-      if (removed.length !== 0) {
-        protohost = trimURLPath(req.url) || ''
-        req.url = protohost + removed + req.url.substr(protohost.length)
-        removed = ''
-      }
-      let layer = stack[index++]
+      const layer = stack[index++]
       if (!layer) {
         setImmediate(done, err)
         return
       }
-      let path = getURLPathname(req.url) || '/'
-      let route = layer.route
-      if (path.toLowerCase().substr(0, route.length) !== route.toLowerCase()) {
+      const path = getURLPathname(req.url) || '/'
+      if (!path.match(layer.route)) {
         return next(err)
       }
-      // skip if route match does not border "/", ".", or end
-      let c = path.length > route.length && path[route.length]
-      if (c && c !== '/' && c !== '.') {
-        return next(err)
-      }
-      // trim off the part of the url that matches the route
-      if (route.length !== 0 && route !== '/') {
-        removed = route
-        protohost = trimURLPath(req.url) || ''
-        req.url = protohost + req.url.substr(protohost.length + removed.length)
-        // ensure leading slash
-        if (!protohost && req.url[0] !== '/') {
-          req.url = '/' + req.url
-          slashAdded = true
-        }
-      }
-      // call the layer handle
-      return call(layer.handle, route, err, req, res, next)
+      return call(layer.handle, layer.route, err, req, res, next)
     }
     await next()
   }
